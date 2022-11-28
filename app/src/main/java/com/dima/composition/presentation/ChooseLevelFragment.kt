a6324f0b1935fb53fc514df52e9b154adaa7702f
Dima <soloveid87@mail.ru>
2022-11-28 13:54:50 +1000

    binding in fragments

--- app/src/main/java/com/dima/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/dima/composition/presentation/ChooseLevelFragment.kt
@@ -6,13 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dima.composition.R
+import com.dima.composition.databinding.FragmentChooseLevelBinding
+import com.dima.composition.databinding.FragmentWelcomeBinding
 
 class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
